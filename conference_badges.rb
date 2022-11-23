7d8dca1de86c3294b32a02320a7ad11c8d67c152
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-11-23 12:14:37 -0500

    commit tree 97c86815272dbb5894a5f25e3e6698221c217c05

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+test_result = badge_maker("Arel")
+puts test_result
+
+def batch_badge_creator(attendees)
+    attendees.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(attendees)
+    room = 0
+    attendees.map do |name|
+        room += 1
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(attendees)
+    puts batch_badge_creator attendee
+    puts assign_rooms attendee
+
+    end
+  
+end
